0030616d7ab794007012cb83dd9595a3d508d000
João Paulo Just Peixoto <just1982@gmail.com>
2018-02-03 10:19:00 -0300

    Fix width calculation: latitude must be converted to radians.

--- src/OSM2MobSinkApp.cpp
+++ src/OSM2MobSinkApp.cpp
@@ -1,6 +1,6 @@
 /*
  * OpenStreetMap to MobSink conversion tool.
- * Copyright (C) 2017-2018 João Paulo Just Peixoto <just1982@gmail.com>
+ * Copyright (C) 2017 - 2018 João Paulo Just Peixoto <just1982@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
